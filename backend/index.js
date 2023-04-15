018c53a3e6d1c1597fd44381c0ac6493ff731432
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-04-15 18:36:13 +0500

    uri

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 import MainPage from "./routes/index.js"
 import cors from "cors"
 const app = express()
-
+const uri = 'mongodb+srv://ddonierov96:<password>@data.l0xaeva.mongodb.net/?retryWrites=true&w=majority'
 
 app.use("/routes/uploads", express.static('./routes/uploads/'))
 app.use(cors({
@@ -17,6 +17,9 @@ app.use('/api', MainPage)
 
 function Server() {
     const PORT = process.env.PORT ?? 8000
+    mongoose.connect(uri)
+        .then(res => console.log('MongoDB connected'))
+        .catch((err) => console.log("MongoDB can't connect , because " + err))
     app.listen(PORT, () => {
         console.log("Server are running on port " + PORT);
     })
